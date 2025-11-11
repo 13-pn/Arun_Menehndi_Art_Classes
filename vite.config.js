698065e7b034fe9626245d6ab4118b6e88204a07
13-pn <pk0451489@gmail.com>
2025-11-12 01:42:18 +0530

    fix: remove static-copy plugin for clean deploy

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: 'public/hero/*', dest: 'hero' },
-        { src: 'public/img/*', dest: 'img' },
-        { src: 'public/enh/*', dest: 'enh' },
-        { src: 'public/Payment_Img/*', dest: 'Payment_Img' },
-        { src: 'public/wcu/*', dest: 'wcu' },
-        { src: 'public/WP/*', dest: 'WP' },
-      ],
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-})
+  plugins: [react()],
+});
